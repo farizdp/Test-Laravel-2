54cb13212c643807a16a23522cafab1e7d50f8aa
farizdp <farizdwipratama@gmail.com>
2020-07-02 02:09:14 +0700

    Initial commit

---
+++ resources/views/items/create.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte.master')
+
+@section('content')
+    <h2>Create</h2>
+@endsection
+
+@push('scripts')
+    <script>
+        var wrappers = document.getElementsByClassName('wrappers'); 
+        var items = ['itu', 'ini'];
+    </script>
